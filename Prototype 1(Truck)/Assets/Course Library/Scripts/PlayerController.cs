2163dd5d8b1bc2d3d46b4dbfc96fb01ca26ddeea
GarlicSuter <markasuter@gmail.com>
2024-02-22 08:40:24 -0500

    Challenge 1 in progress

--- Prototype 1(Truck)/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 1(Truck)/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
+    private float speed = 10.0f;
+    public float turnSpeed = 10.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
