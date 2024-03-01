d16c3428cf4883b7cd0231d144848d64d79ed138
GarlicSuter <markasuter@gmail.com>
2024-03-01 11:00:38 -0500

    2.2 finished

--- Prototype2-FeedAnimals/Assets/Scripts/PlayerController.cs
+++ Prototype2-FeedAnimals/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject steakPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //spawn steak
+            Instantiate(steakPrefab, transform.position, steakPrefab.transform.rotation);
+        }
+        
         //keep the player inbounds
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
