2e16223b31189eea5d69c4c63e61f6f267109ff7
GarlicSuter <markasuter@gmail.com>
2024-03-14 14:41:11 -0400

    Challenge 2 done

--- Prototype2-FeedAnimals/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype2-FeedAnimals/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float fireDelay = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
+        fireDelay -= Time.deltaTime;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && fireDelay <= 0.0f)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            fireDelay = 1.0f;
         }
     }
 }
